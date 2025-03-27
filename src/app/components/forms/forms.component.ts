2e03b52fa258c45642158e75a4b8582473d13bff
Mayara Santos Rocha Cortes <mayara.cortes@mj.gov.br>
2025-03-27 12:33:35 -0300

    Feat: create form structure and style

---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  imports: [],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss'
+})
+export class FormsComponent implements OnInit {
+
+  results = [];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {}
+}
